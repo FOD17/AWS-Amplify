809fe9df7a4ff1f3c4af70e16e93b732144e2833
FOD17 <w_mark81@yahoo.com>
2021-01-31 11:00:16 -0600

    added auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <h1>Shoving this directly into master</h1>
+      <AmplifySignOut />
     </div>
   );
 }
